b3eccd2afb822b0178350e9bdc3086c1037ba2de
Mirza <mirza@Mirzas-MBP.lan>
2019-03-11 21:39:11 -0400

    first commit

---
+++ precache-manifest.70d5ba85f7bf6036b89a4f6adb9c30a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "79191e18a557d0196c0b",
+    "url": "/Hackernews/static/css/main.c786ad48.chunk.css"
+  },
+  {
+    "revision": "79191e18a557d0196c0b",
+    "url": "/Hackernews/static/js/main.79191e18.chunk.js"
+  },
+  {
+    "revision": "98c26074601aab963765",
+    "url": "/Hackernews/static/js/runtime~main.98c26074.js"
+  },
+  {
+    "revision": "15d61738c2449ee58912",
+    "url": "/Hackernews/static/js/2.15d61738.chunk.js"
+  },
+  {
+    "revision": "cb14a2f3a46c72b7e00642e7ad1c46a9",
+    "url": "/Hackernews/index.html"
+  }
+];
